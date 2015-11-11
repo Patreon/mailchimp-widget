e01cbb04b55e2a88c4e255bac1edf6dbb02e2204
David Kettler <21echoes@gmail.com>
2015-11-11 10:29:34 -0800

    get it working with the MailChimp API

---
+++ mac/bundler.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp mailchimp.py Update\ MailChimp\ for\ Patrons.app/Contents/mailchimp.py;
+mkdir Update\ MailChimp\ for\ Patrons.app/Contents/Resources/Config;
+cp config_example.csv Update\ MailChimp\ for\ Patrons.app/Contents/Resources/Config/config.csv;
